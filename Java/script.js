381359129af3fbcffd3143107459bbe4615562a7
Prince Sein <seinprince2@gmail.com>
2024-09-10 11:12:29 +0200

    added js script learning

---
+++ Java/script.js
@@ -0,0 +1,41 @@
+// document.write("<h1 style='color:blue; text-align:center;'>")
+// document.write("Welcome to Java Script😒<br>after the paragraph </h1>")
+
+// alert("Welcome to the class")
+// if(confirm("Do you want to proceed?")){
+//     alert("processing");
+// }else{
+//     alert("Ok byee")
+// }
+// let name = window.prompt("Enter name: ");
+//  document.writeln("Hello "+name+ ", Welcome to js script class");
+
+// document.querySelector("html").addEventListener("click", function () {
+//     alert("Ouch! Stop poking me!");
+//   });
+
+// document.querySelector("button").addEventListener("click", function () {
+//     console.log("You just pressed the button")
+//   alert("Ouch! Stop pressing me!");
+// });
+
+// const  x=5;
+// x="jet";
+// console.log(typeof x);
+
+// function before(){
+//   console.log(age);
+//   var age = 27;
+//   console.log(age);
+// }
+
+// before();
+var x="5";
+var y=5;
+
+if(y==y){
+  console.log("true");
+}
+else{
+  console.log("false");
+}
